ae8e94adfdb1d5b4b91b14a14defe0b6badd02db
wilder09 <aadityapanchal289@gmail.com>
2024-06-10 13:50:15 -0500

    added subpages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app=Flask(__name__)
 
@@ -6,13 +6,25 @@ app=Flask(__name__)
 def index(): 
   return render_template('index.html')
 
-@app.route('/Contact')
-def contact():
-  return render_template('Contact.html')
+@app.route('/About')
+def about():
+  return render_template('About.html')
 
 @app.route('/Projects')
 def project():
   return render_template('Projects.html')
+
+@app.route('/Webdev')
+def webdev():
+  return render_template('Webdev.html')
+
+@app.route('/Design')
+def design():
+  return render_template('Design.html')
+
+@app.route('/Marketing')
+def market():
+  return render_template('Marketing.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
