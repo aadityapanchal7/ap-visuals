63706ff96611fadcbd7015942ba2a424382431c7
wilder09 <aadityapanchal289@gmail.com>
2024-06-09 22:57:52 -0500

    added contact subpage

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app=Flask(__name__)
 def index(): 
   return render_template('index.html')
 
+@app.route('/Contact')
+def contact():
+  return render_template('Contact.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
